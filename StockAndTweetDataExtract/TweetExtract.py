b4f31317aacded5fa51ca67a9fa991914e7969a7
FacVain <assassin_ezio55@hotmail.com>
2022-10-21 11:39:18 +0300

    Collected Bist100 tweets of the last 10 years
    Created TweetExtract script that takes a year as an argument and scrapes
    tweets with hashtag Bist100 that were posted in the given year.

---
+++ StockAndTweetDataExtract/TweetExtract.py
@@ -0,0 +1,38 @@
+import sys
+import snscrape.modules.twitter as sntwitter
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import pandas as pd
+import multiprocessing
+from pathlib import Path
+
+
+def extract_tweet(since, until, thread_num):
+    t_query = f'bist100 since:{since.strftime("%Y-%m-%d")} until:{until.strftime("%Y-%m-%d")}'
+    tweets = []
+    for tweet in sntwitter.TwitterSearchScraper(t_query).get_items():
+        tweets.append([tweet.date, tweet.user.username, tweet.content])
+    t_df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet']).set_index('Date')
+    filepath = Path(f'./{since.year}/tweet_part{thread_num}.csv')
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    t_df.to_csv(filepath)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        sys.exit('Enter year as an argument.')
+    year = int(sys.argv[1])
+    init_date = date(year, 1, 1)
+    end_date = date(year, 12, 1)
+    pnum = 0
+    processes = []
+    while init_date < end_date:
+        s_date = init_date + relativedelta(months=2)
+        processes.append(multiprocessing.Process(target=extract_tweet, args=(init_date, s_date, pnum,)))
+        processes[pnum].start()
+        print(f'Process {init_date.strftime("%Y-%m-%d")} started')
+        init_date = s_date
+        pnum += 1
+
+    for process in processes:
+        process.join()
